4678ad4c92b9b27903f0f22cfdc93c548bb68b33
alfredobaetti <baettialfredo@gmail.com>
2021-09-21 01:29:10 -0300

    falta descargare los pdf

--- final.py
+++ final.py
@@ -1,15 +1,18 @@
 from RPA.Browser.Selenium import Selenium
 from RPA.Excel.Files import Files
 from RPA.PDF import PDF
+from RPA.Tables import Tables
 import time
 import os
 import re
+import pandas as pd
 #from constants import DOWNLOAD_DIR, URL
 
 
 driver = Selenium()
 excel = Files()
 pdf = PDF()
+tables = Tables()
 
 def click_dive_in(url):
     driver.open_chrome_browser(url)
@@ -44,11 +47,41 @@ def write_amounts_to_excel(info):
 def scrape_table_agency():
     element = driver.find_element('//*[@id="agency-tiles-widget"]/div/div[9]/div[1]/div/div/div/div[2]/a')
     element.click()
-    driver.wait_until_element_is_visible('//*[@id="investments-table-object_length"]/label/select',timeout=15)
-    entries = driver.find_element('//select[@name="investments-table-object_length"]')
-    entries.select_from_list_by_index(3) 
-    time.time(15)
 
+    driver.wait_until_element_is_visible('//*[@id="investments-table-object_length"]/label/select',timeout=15)
+    driver.click_element_when_visible('//select[@name="investments-table-object_length"]')
+    driver.click_element_when_visible('//*[@id="investments-table-object_length"]/label/select/option[4]')
+
+    time.sleep(10)
+
+    inv_table = driver.find_element('//table[@id="investments-table-object"]')
+    
+    write_table_to_excel(inv_table)
+
+
+def write_table_to_excel(table):
+    tab_html = table.get_attribute('outerHTML')
+    tab_dfs = pd.read_html(tab_html)
+
+    file = excel.open_workbook(f"challenge.xlsx")
+    file.create_worksheet("Individual Investments")
+    # df = pd.DataFrame(columns=tab_dfs[0].columns)
+
+    # for row in tab_dfs[0].iterrows:    
+    #     df.append(row)
+
+    #file.append_worksheet("Individual Investments", tab_dfs[0])
+
+    with pd.ExcelWriter('challenge.xlsx', mode='a') as writer:  
+        tab_dfs[0].to_excel(writer, sheet_name='Individual Investments')
+
+    time.sleep(15)
+
+def download_UII_pdf():
+    UII = driver.find_element('//*[@id="investments-table-object"]/tbody/tr/td[1]/a')
+
+    for i in range(len(UII)):
+        UII[i].click()
 
 
 
@@ -59,7 +92,7 @@ def main():
         click_dive_in(url)
         scrape_agencies_amounts()
         scrape_table_agency()
-        #download_UII_pdf()
+        download_UII_pdf()
     finally:
         driver.close_all_browsers()
 
