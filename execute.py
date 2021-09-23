bd6891cf82386209f96f5e0af96a5986e09db393
alfredobaetti <baettialfredo@gmail.com>
2021-09-23 00:57:39 -0300

    funcionando

---
+++ execute.py
@@ -0,0 +1,18 @@
+import os
+from re import U
+from itdashboard import Challenge
+from agencies import ID
+
+def main():
+    challenge = Challenge(url="https://itdashboard.gov/")
+    try:
+        challenge.click_dive_in()
+        challenge.scrape_agencies_amounts()
+        challenge.scrape_table_agency(ID)
+        challenge.download_UII_pdf()
+    finally:
+        challenge.close_all_browsers()
+
+
+if __name__ == "__main__":
+    main()
---
+++ itdashboard.py
@@ -0,0 +1,119 @@
+from RPA.Browser.Selenium import Selenium
+from RPA.Excel.Files import Files
+#from RPA.PDF import PDF
+import time
+import os
+import re
+import pandas as pd
+
+
+class Challenge:
+
+    agencies_info = {}
+    uii_links = []
+
+    def __init__(self, url):
+        self.driver = Selenium()
+        self.excel = Files()
+
+        self.driver.set_download_directory(os.path.join(os.getcwd(), f"output/"))
+        self.driver.open_available_browser(url)
+        self.driver.maximize_browser_window()
+
+    def click_dive_in(self):
+        self.driver.wait_until_page_contains('DIVE IN')
+        self.driver.find_element('//a[@class="btn btn-default btn-lg-2x trend_sans_oneregular"]').click()
+
+    def scrape_agencies_amounts(self):
+        time.sleep(5)
+        AGENCIES = []
+        AMOUNTS = []
+
+        agencies = self.driver.find_elements('//div[@id="agency-tiles-widget"]//span[@class="h4 w200"]')
+        amounts = self.driver.find_elements('//div[@id="agency-tiles-widget"]//span[@class=" h1 w900"]')
+
+        for i in range(len(agencies)):
+            AGENCIES.append(agencies[i].text)
+            AMOUNTS.append(amounts[i].text)
+
+        self.agencies_info = {'Agencie': AGENCIES, 'Amount': AMOUNTS}
+        self.write_amounts_to_excel()
+
+    def write_amounts_to_excel(self):
+        file = self.excel.create_workbook(f"output/challenge.xlsx")
+
+        file.append_worksheet("Sheet", self.agencies_info)
+        file.rename_worksheet("Agencies", "Sheet")
+        file.save()
+
+
+    def scrape_table_agency(self, agency_id):
+        try:
+            self.driver.find_elements('//*[@id="agency-tiles-widget"]/div/div/div/div/div/div')[agency_id].click()
+        except:
+            print("Agency was not found")
+
+        while True:
+            try:
+                self.driver.click_element_when_visible('//select[@name="investments-table-object_length"]')
+                self.driver.click_element_when_visible('//*[@id="investments-table-object_length"]/label/select/option[4]')
+                break
+            except:
+                pass
+
+        while True:
+            if self.driver.find_element('investments-table-object_next').get_attribute("class") == 'paginate_button next disabled':
+                inv_table = self.driver.find_element('//table[@id="investments-table-object"]')
+                self.write_table_to_excel(inv_table)
+                break
+
+
+    def write_table_to_excel(self,table):
+        tab_html = table.get_attribute('outerHTML')
+        tab_dfs = pd.read_html(tab_html)
+
+        file = self.excel.open_workbook(f"challenge.xlsx")
+        file.create_worksheet("Individual Investments")
+        # df = pd.DataFrame(columns=tab_dfs[0].columns)
+
+        # for row in tab_dfs[0].iterrows:    
+        #     df.append(row)
+
+        #file.append_worksheet("Individual Investments", tab_dfs[0])
+
+        with pd.ExcelWriter('challenge.xlsx', mode='a') as writer:  
+            tab_dfs[0].to_excel(writer, sheet_name='Individual Investments')
+
+
+    def get_uii_links(self):
+        all_uiis = self.driver.find_elements('//*[@id="investments-table-object"]/tbody/tr/td[1]/a')
+        for uii in all_uiis:
+            self.uii_links.append(uii.get_attribute("href"))
+
+
+    def download_UII_pdf(self):
+        self.get_uii_links()
+
+        for link in self.uii_links:
+
+            self.driver.go_to(link)
+
+            self.driver.click_element_when_visible('//*[@id="business-case-pdf"]/a')
+
+            while True:
+                try:
+                    time.sleep(1)
+                    if self.driver.find_element('//div[@id="business-case-pdf"]').find_element_by_tag_name("span"):
+                        time.sleep(1)
+                    else:
+                        break
+                except:
+                    if self.driver.find_element('//*[contains(@id,"business-case-pdf")]//a[@aria-busy="false"]'):
+                        time.sleep(1)
+                        break
+
+
+    def close_all_browsers(self):
+        self.driver.close_all_browsers()
+
+
